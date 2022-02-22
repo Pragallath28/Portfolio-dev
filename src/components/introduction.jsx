3963d4935edc54bcbb51eee515e7fa3b53579f99
Pragallath <53934292+Pragallath28@users.noreply.github.com>
2022-02-22 11:32:41 +0530

    Commit all

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -29,23 +29,8 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
+                          <h1>I love Making Different New<br /> THINGS !!</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://github.com/Pragallath28" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
                         </div>
                       </div>
                     </div>
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -8,7 +8,7 @@ export default class Timeline extends Component {
           <div className="colorlib-narrow-content">
             <div className="row">
               <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">highlights</span>
+                <span className="heading-meta">Highlights</span>
                 <h2 className="colorlib-heading animate-box">Timeline</h2>
               </div>
             </div>
@@ -21,11 +21,11 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Appin Technology Lab<span>August 2020-September 2020</span></h2>
+                        <h2>Internship at Appin Technology Lab<span>August 2020 - September 2020</span></h2>
                         <p>I spent around one month Learning to design a webpage and creating website.</p>
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at Micrsoft KSI (Knowledge Solution India) <span>August 2020-September 2020</span></h2>
+                        <h2>Internship at Micrsoft KSI (Knowledge Solution India) <span>October 2020 - December 2020</span></h2>
                         <p>I spent around two months in Online Banking Portal. The project is based on the banking system 
                         through online.It is designed to show the amount, balance, customer
                         related information and other services provided are to deposit money, transfer money and also to close the
@@ -51,7 +51,7 @@ export default class Timeline extends Component {
                       </div>
                       <div className="timeline-label">
                         <h2>Primary and Higher Education <span>2018</span></h2>
-                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 67.75. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
                       </div>
                     </div>
                   </article>
