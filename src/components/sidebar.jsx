f19c55dcf900c6451681d5c48f1dfbf326f39527
Pragallath <53934292+Pragallath28@users.noreply.github.com>
2022-03-16 15:12:13 +0530

    commit on master

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -25,8 +25,6 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                {/* <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li> */}
                 <li><a href="https://www.instagram.com/pragallath.28/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
                 <li><a href="https://www.linkedin.com/in/pragallath-ps-bb0ba3193/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
                 <li><a href="https://github.com/Pragallath28" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
